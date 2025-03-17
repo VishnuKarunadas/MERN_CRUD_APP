81be596a4dc9abf0b62eacb30734ac518b09c550
VishnuKarunadas <vishnumk1090@gmail.com>
2025-03-17 11:09:22 +0530

    client side added,tailwind config,vite installed

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className=' flex-1/2'>
+      
+      <h1 className='flex-wrap'>Test</h1>
+      <h4 className='flex'>Name</h4>
+      
+      
+      </div>
+    </>
+  )
+}
+
+export default App
